4e63cba3b686b00a96d0d7703273c61232aa8135
Felipe Alafy <f.alafyrsilva@hotmail.com>
2020-12-17 10:04:23 -0300

    Criptografia usando o jasyspt, adicionado campo para cpf na MainGUI, no Banco de dados, adcionado string para cpf nas classes clientes e clientesfrontend, feito as alterações para suporte do cpf na classe controller, e nas classes MainGUI

--- starnet-cdc/src/starnet/cdc/backend/Encripta.kt
+++ starnet-cdc/src/starnet/cdc/backend/Encripta.kt
@@ -5,9 +5,11 @@ Created on 20/11/2020
  */
 package starnet.cdc.backend
 
+import org.jasypt.util.text.BasicTextEncryptor
 import sun.misc.BASE64Encoder
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
+import javax.swing.plaf.basic.BasicArrowButton
 
 class Encripta {
     fun encriptar(senha:String): String {
@@ -20,5 +22,19 @@ class Encripta {
             ns.printStackTrace()
             senha
         }
+    }
+
+    fun encriptar2(toEncrypt:String):String {
+        val bte = BasicTextEncryptor()
+        bte.setPasswordCharArray(("QuTFMViKiWdWABimyM1GRnLDqjtapACXMm8V1hiqDcQFivlZLYAwObBg9Gl4KCrqjINovC62g2nnemAqrewYZ5z8Q6CLdggl" +
+                "Y9VsQhRkhYzCcYQtP6RpSXR1odFmca9NRomT9VYUmaLMhMunb9y8jQ8cCkn44WlpNJBt3ORVPFjVcEOzKfJAeHg0xoTT2NYJ7ZkU3S5J").toCharArray())
+        return bte.encrypt(toEncrypt)
+    }
+
+    fun descriptografar(toDecrypt:String):String {
+        val bte = BasicTextEncryptor()
+        bte.setPasswordCharArray(("QuTFMViKiWdWABimyM1GRnLDqjtapACXMm8V1hiqDcQFivlZLYAwObBg9Gl4KCrqjINovC62g2nnemAqrewYZ5z8Q6CLdggl" +
+                "Y9VsQhRkhYzCcYQtP6RpSXR1odFmca9NRomT9VYUmaLMhMunb9y8jQ8cCkn44WlpNJBt3ORVPFjVcEOzKfJAeHg0xoTT2NYJ7ZkU3S5J").toCharArray())
+        return bte.decrypt(toDecrypt)
     }
 }
